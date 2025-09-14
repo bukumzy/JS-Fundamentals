8e607900528be74b90ff3c89c9f4b941c8c64228
Oluwabukunmi Moronfolu <kumzyd@gmail.com>
2025-09-14 11:15:23 +0100

    to integers

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 const integ = process.argv[2];
 if (isNaN(parseInt(integ))) {
     console.log("Not a number");
-} else if (integ >= 1) { 
-console.log("My number: " + parseInt(integ))
+} else { 
+console.log("My number: " + parseInt(integ));
 };
