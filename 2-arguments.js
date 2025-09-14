3961c4dafa8b64ac4aae1ea6432941abdb3468e3
Oluwabukunmi Moronfolu <kumzyd@gmail.com>
2025-09-14 06:44:39 +0100

    arguments added

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const argv = process.argv.slice(2);
+if (argv.length == 0) {
+    console.log("No argument");
+} else if (argv.length == 1) {
+    console.log("Argument found");
+}else {
+    console.log("Arguments found");
+}
